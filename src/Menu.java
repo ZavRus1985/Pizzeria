895f95807f5e821b5f8aa8199103ff5433b2ca89
User <zavodilin-ru@yandex.ru>
2022-02-16 08:29:52 +0300

    Initial commit

---
+++ src/Menu.java
@@ -0,0 +1,24 @@
+public class Menu {
+
+ void printCheck() {
+
+  System.out.println("Выбери пиццу:");
+  System.out.println("Грибная - 550R");
+  System.out.println("Сырная - 400R");
+  System.out.println("Европейская - 629R");
+  System.out.println("Супер Пепперони - 729R");
+  System.out.println("Пепперони - 620R");
+
+
+
+ }
+
+}
+       /*
+        1) Грибная - 550R
+        2) Сырная - 400R
+        3) Европейская - 629R
+        4) Супер Пепперони - 729R
+        5) Пепперони - 620R
+
+        */
---
+++ src/Pizza.java
@@ -0,0 +1,4 @@
+public class Pizza {
+    String name;
+    int price;
+}

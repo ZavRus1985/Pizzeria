22624c6af7ae37348289df1b79e8434ef87b0857
ZavRus1985 <95374736+ZavRus1985@users.noreply.github.com>
2022-02-22 21:32:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Menu pr = new Menu();
-        pr.printCheck();
+       Pizza pizza1 = new Pizza("Грибная", 550);
+       Pizza pizza2 = new Pizza("Сырная",400);
+       Pizza pizza3 = new Pizza("Европейская", 629);
+       Pizza pizza4 = new Pizza("Супер Пепперони", 729);
+       Pizza pizza5 = new Pizza("Пепперони", 620);
 
+       Calculator calculator = new Calculator();
+       int i = calculator.sum(pizza1, pizza2, pizza3, pizza4, pizza5);
+       Pizza pizzaX = new Pizza("Итого", i);
+       pizzaX.dataOfPizza();
     }
 }

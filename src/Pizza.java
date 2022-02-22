cdbb58f6deb58857a6036ba7c63ba99d1a89eb87
ZavRus1985 <95374736+ZavRus1985@users.noreply.github.com>
2022-02-22 21:31:03 +0300

    Update Pizza.java

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,15 @@
 public class Pizza {
     String name;
     int price;
+
+    public Pizza(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    void dataOfPizza(){
+        System.out.println(name + " " + price);
+
+    }
 }
